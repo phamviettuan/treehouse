8324752e51e1b4af072a3cbb427285abeac86795
phamviettuan <tuan.pham@atansolutions.com>
2013-12-16 18:37:15 -0500

    treehouse

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treehouse::Application.config.secret_token = '8b53fcca0a8dc0b19fd0fbe90d2027de4933c8105beb0cc96a2d319e7ef18df06de888e7e6cf4d55cf415d47da3bb82b8f0599bee1ce0d9178b703937a42d8d4'
