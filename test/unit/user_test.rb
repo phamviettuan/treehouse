36e001f5d6b0e742100dd021f8458d3c215ed6ac
phamviettuan <tuan.pham@atansolutions.com>
2014-01-02 11:44:23 -0500

    test profile page

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -22,7 +22,6 @@ class UserTest < ActiveSupport::TestCase
  test "user should have a unique profile name" do
  	user = User.new
  	user.profile_name = users(:tuan).profile_name
-    
  	assert !user.save
  	assert !user.errors[:profile_name].empty?
  end
@@ -30,11 +29,17 @@ class UserTest < ActiveSupport::TestCase
  test "user should have a profile name without spaces" do
  	user = User.new
  	user.profile_name = "My profile with spaces"
-
  	assert !user.save
  	assert !user.errors[:profile_name].empty?
  	assert  user.errors[:profile_name].include?("Must be formatted correctly!")
+ end
 
+ test "a user can have a correctly formatted profile name" do
+ 	user = User.new(first_name: "Tuan", last_name: "Pham", email: "tuanp1711@gmail.com")
+ 	user.password = user.password_confirmation = "abcde"
+
+ 	user.profile_name = "tuanpham"
+ 	assert user.valid?
  end
 end
 
